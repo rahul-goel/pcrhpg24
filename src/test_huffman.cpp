693d5c6fee5c2e9c8e9543a1babe1cf1c8a5a493
Rahul Goel <rahulgoellko@gmail.com>
2023-11-20 13:44:17 +0530

    test for huffman

--- src/test_huffman.cpp
+++ src/test_huffman.cpp
@@ -17,22 +17,40 @@ template<typename T, typename udtype>
 void test_huffman(string T_str, string udtype_str, int num_points) {
   vector<T> data;
   for (int i = 0; i < num_points; ++i) {
-    data.push_back(rand() % 10);
+    data.push_back(rand() % 10000);
   }
 
+  // Huffman<T> hfmn;
+  // hfmn.calculate_frequencies(data);
+  // hfmn.constraint_table_size(4096);
+  // hfmn.generate_huffman_tree();
+  // hfmn.create_dictionary();
+  // hfmn.clear_huffman_tree();
+
+  // auto collapsed_dict = hfmn.template get_collapsed_dictionary<udtype>();
+  // vector<pair<T,int>> decoder_table = hfmn.get_gpu_huffman_table();
+
+  // vector<udtype> encoded = hfmn.template compress_udtype_subarray_fast<udtype, typename vector<T>::iterator>(data.begin(), data.end(), collapsed_dict);
+  // vector<T> decoded(data.size());
+  // hfmn.template decompress_udtype_subarray_fast<udtype, typename vector<T>::iterator>(decoded.begin(), decoded.end(), encoded, decoder_table);
+
+  int table_size = 4096;
   Huffman<T> hfmn;
   hfmn.calculate_frequencies(data);
-  hfmn.constraint_table_size(4096);
   hfmn.generate_huffman_tree();
-  hfmn.create_dictionary();
+  auto dict = hfmn.template create_dictionary_pjn<udtype>(table_size);
+  auto table = hfmn.template get_gpu_huffman_table_pjn<udtype>(dict, table_size);
   hfmn.clear_huffman_tree();
 
-  auto collapsed_dict = hfmn.template get_collapsed_dictionary<udtype>();
-  vector<pair<T,int>> decoder_table = hfmn.get_gpu_huffman_table();
+  // auto encoded = hfmn.template compress_udtype_subarray_fast_pjn_idea<udtype, it_type, int32_t>(data.begin(), data.end(), dict);
+  auto encoded = hfmn.template compress_udtype_subarray_fast_pjn_idea<
+    udtype, typename vector<T>::iterator, T>(data.begin(), data.end(),
+                                             dict, table_size);
 
-  vector<udtype> encoded = hfmn.template compress_udtype_subarray_fast<udtype, typename vector<T>::iterator>(data.begin(), data.end(), collapsed_dict);
   vector<T> decoded(data.size());
-  hfmn.template decompress_udtype_subarray_fast<udtype, typename vector<T>::iterator>(decoded.begin(), decoded.end(), encoded, decoder_table);
+  hfmn.template decompress_udtype_subarray_fast_pjn_idea<
+      udtype, typename vector<T>::iterator, T>(
+      decoded.begin(), decoded.end(), encoded.first, encoded.second, table);
 
   bool equal = (decoded == data);
   {
@@ -130,20 +148,20 @@ int main(int argc, char *argv[]) {
   int num_points = atoi(argv[1]);
   assert(num_points > 0);
 
-  multithreaded_huffman<int32_t, uint32_t>("int32_t", "uint32_t", num_points);
+  // multithreaded_huffman<int32_t, uint32_t>("int32_t", "uint32_t", num_points);
 
   vector<thread> threads;
-  // threads.push_back(std::move(thread(test_huffman<char, uint32_t>, "char", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<unsigned char, uint32_t>, "unsigned char", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<short, uint32_t>, "short", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<int8_t, uint32_t>, "int8_t", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<int16_t, uint32_t>, "int16_t", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<int32_t, uint32_t>, "int32_t", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<int64_t, uint32_t>, "int64_t", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<uint8_t, uint32_t>, "uint8_t", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<uint16_t, uint32_t>, "uint16_t", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<uint32_t, uint32_t>, "uint32_t", "uint32_t", num_points)));
-  // threads.push_back(std::move(thread(test_huffman<uint64_t, uint32_t>, "uint64_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<char, uint32_t>, "char", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<unsigned char, uint32_t>, "unsigned char", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<short, uint32_t>, "short", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<int8_t, uint32_t>, "int8_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<int16_t, uint32_t>, "int16_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<int32_t, uint32_t>, "int32_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<int64_t, uint32_t>, "int64_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<uint8_t, uint32_t>, "uint8_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<uint16_t, uint32_t>, "uint16_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<uint32_t, uint32_t>, "uint32_t", "uint32_t", num_points)));
+  threads.push_back(std::move(thread(test_huffman<uint64_t, uint32_t>, "uint64_t", "uint32_t", num_points)));
 
   for (thread &t : threads) t.join();
   return 0;
