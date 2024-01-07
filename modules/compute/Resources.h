554ff59d2ba7ce3addc712232d3fbd95a05606f9
Rahul Goel <rahulgoellko@gmail.com>
2024-01-07 14:59:45 +0530

    1024 threads + switch to double for close-by points

--- modules/compute/Resources.h
+++ modules/compute/Resources.h
@@ -1,9 +1,9 @@
 
 #pragma once
 
-#define POINTS_PER_THREAD 512
-#define CLUSTERS_PER_THREAD 4
-#define WORKGROUP_SIZE 128
+#define POINTS_PER_THREAD 64
+#define CLUSTERS_PER_THREAD 1
+#define WORKGROUP_SIZE 1024
 
 #define POINTS_PER_WORKGROUP (POINTS_PER_THREAD * WORKGROUP_SIZE)
 // Adjust this to be something in the order of 1 million points
